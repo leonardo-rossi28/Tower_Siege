65a0e3c04260c1387dec1debfa21bd42595d9bf2
elia102005 <elia.naldini@studio.unibo.it>
2026-05-13 23:04:10 +0200

    I've almost finished the Tower class; I'm just missing the attack method used to attack enemies. However, it uses a new Projectile class that I've only just created.

---
+++ app/src/main/java/it/unibo/TowerSiege/model/Projectile.java
@@ -0,0 +1,5 @@
+package it.unibo.TowerSiege.model;
+
+public class Projectile {
+    
+}
--- app/src/main/java/it/unibo/TowerSiege/model/Tower.java
+++ app/src/main/java/it/unibo/TowerSiege/model/Tower.java
@@ -1,5 +1,7 @@
 package it.unibo.TowerSiege.model;
 
+
+
 public class Tower {
     
     private final TowerType type;
@@ -9,24 +11,61 @@ public class Tower {
     private boolean alive;
     private int cooldownTicks; 
 
-    public Tower(TowerType type) {  //constructor
+    public Tower(TowerType type) {  
         this.type=type;
-        this.level=1;  //set the tower's level initially to 1
+        this.level=1;  
         this.alive=true;
-        this.cooldownTicks=0; //cooldawnTick=0 means that the tower has the ability
+        this.cooldownTicks=0; 
     }
 
-    public void setPosition(double x, double y){
+    public void Position(double x, double y){
         this.pixelX=x;
         this.pixelY=y;
     }
 
-    public void tick(){  //decrease the countdown to get the ability
+    public void tick(){  
         if(cooldownTicks > 0){
             cooldownTicks--;
         }
     }
 
+    public boolean isEnemyInRange(Enemy enemy){
+        double dx= enemy.getPixelX()-pixelX;
+        double dy= enemy.getPiexelY() - pixelY;
+        double distSq= dx*dx + dy*dy;
+        double rangePx= type.getRange() * 40;
+        return distSq <= (rangePx * rangePx);
+    }
+
+    public void upgrade(){
+        this.level++;
+    }
+
+    public TowerType getType(){
+        return type;
+    }
+    public double getPixelX(){
+        return pixelX;
+    }
+
+    public double getPixelY(){
+        return pixelY;
+    }
+
+    public int getLevel(){
+        return level;
+    }
+
+    public boolean isAlive(){
+        return alive;
+    }
+
+    public int getRange(){
+        return type.getRange();
+    }
+     public int getDamage(){
+        return type.getDamage() + (level -1)*2;
+     }
     
 
 

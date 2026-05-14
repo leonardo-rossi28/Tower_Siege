fca7894210d1d890dc943a1d9a917f3cf4b05cd6
elia102005 <elia.naldini@studio.unibo.it>
2026-05-14 10:46:30 +0200

    Ended Projectile.java.

--- src/main/java/it/unibo/TowerSiege/model/Projectile.java
+++ src/main/java/it/unibo/TowerSiege/model/Projectile.java
@@ -1,5 +1,10 @@
 package it.unibo.TowerSiege.model;
 
+/**
+ * This class represent a projectile that moves towards the target
+ * The projectile hit if the enemy is in the same pixel otherwise the 
+ * projectile continues to move.
+ */
 public class Projectile {
     private final Tower source;
     private final Enemy target;
@@ -7,6 +12,7 @@ public class Projectile {
     private double y;
     private final double speed=15.0;
     private boolean alive=true;
+
 
     public Projectile(Tower source, Enemy target){
         this.source=source;
@@ -26,8 +32,33 @@ public class Projectile {
 
         double dx=tx-x;
         double dy=ty-y;
-        double dist=Math.sqrt(dx*dx + dy*dy);
+        double dist = Math.sqrt(dx*dx + dy*dy);
 
+        if( dist <= speed){
+            target.takeDamage(source.getDamage());
+            if(source.getType()==TowerType.ICE){
+                target.applySlow(0.5,20);
+            }
+            this.alive=false;
+        }else{
+            this.x +=(dx/dist)*speed;
+            this.y +=(dy/dist)*speed;
+        }
+        
         
     }
+
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
+    public TowerType getSourceTowerType(){
+        return source.getType();
+    }
+    public boolean isAlive(){
+        return alive;
+    }
+
 }

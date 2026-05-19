b19ac65ce138514890abb96cec16c03cf0a11568
elia102005 <elia.naldini@studio.unibo.it>
2026-05-19 11:14:48 +0200

    Fixed TowerImpl and add interface

---
+++ src/main/java/it/unibo/TowerSiege/model/tower/api/Tower.java
@@ -0,0 +1,67 @@
+package it.unibo.TowerSiege.model.tower.api;
+
+import it.unibo.TowerSiege.model.projectile.api.Projectile;
+import it.unibo.TowerSiege.enemy.api.Enemy;
+import it.unibo.TowerSiege.model.tower.TowerType;
+
+
+
+
+public interface Tower {
+
+    /**
+     * @param x
+     * @param y
+     */
+    void setPosition(double x,double y);
+
+    void tick();
+
+    /**
+     * @param enemy
+     * @return true if enemy is in range
+     */
+    boolean isEnemyInRange(Enemy enemy);
+
+    /**
+     * @param enemy
+     * @return Projectile or null if on cooldown or out of range
+     */
+    Projectile attack(Enemy enemy);
+
+
+    /** 
+     * upgrade tower
+     */
+    void upgrade();
+
+    /**
+     * @return tower type
+     */
+    TowerType getType();
+
+    /**
+     * @return pixel x
+     */
+    double getPixelX();
+
+    /**
+     * @return pixel y
+     */
+    double getPixelY();
+
+    /**
+     * @return true if alive
+     */
+    boolean isAlive();
+
+    /**
+     * @return range
+     */
+    int getRange();
+
+    /**
+     * @return damage
+     */
+    int getDamage();
+}
--- src/main/java/it/unibo/TowerSiege/model/tower/impl/TowerImpl.java
+++ src/main/java/it/unibo/TowerSiege/model/tower/impl/TowerImpl.java
@@ -1,9 +1,15 @@
 package it.unibo.TowerSiege.model.tower.impl;
 
-import it.unibo.TowerSiege.model.enemy.impl.Enemy;
+import it.unibo.TowerSiege.model.enemy.api.Enemy;
 import it.unibo.TowerSiege.model.tower.TowerType;
+import it.unibo.TowerSiege.model.tower.api.Tower;
+import it.unibo.TowerSiege.model.projectile.api.Projectile;
+import it.unibo.TowerSIege.model.progecyile.impl.ProjectileImpl;
 
-public class TowerImpl {
+
+
+
+public class TowerImpl implements Tower{
     
     private final TowerType type;
     private double pixelX;
@@ -12,6 +18,7 @@ public class TowerImpl {
     private boolean alive;
     private int cooldownTicks; 
 
+
     public TowerImpl(TowerType type) {  
         this.type=type;
         this.level=1;  
@@ -19,51 +26,95 @@ public class TowerImpl {
         this.cooldownTicks=0; 
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void setPosition(double x, double y){
         this.pixelX=x;
         this.pixelY=y;
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void tick(){  
         if(cooldownTicks > 0){
             cooldownTicks--;
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public boolean isEnemyInRange(Enemy enemy){
         double dx= enemy.getPixelX()-pixelX;
-        double dy= enemy.getPiexelY() - pixelY;
+        double dy= enemy.getPixelY() - pixelY;
         double distSq= dx*dx + dy*dy;
         double rangePx= type.getRange() * 40;
         return distSq <= (rangePx * rangePx);
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void upgrade(){
         this.level++;
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public TowerType getType(){
         return type;
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public double getPixelX(){
         return pixelX;
     }
-
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public double getPixelY(){
         return pixelY;
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public int getLevel(){
         return level;
     }
-
+ù/**
+     * {@inheritDoc}
+     */
+    @Override
     public boolean isAlive(){
         return alive;
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public int getRange(){
         return type.getRange();
     }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
      public int getDamage(){
         return type.getDamage() + (level -1)*2;
      }
